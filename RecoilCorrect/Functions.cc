14a1b8aec11f6f3d3c7081eb630590388b1029d9
Yongbin Feng <ybfeng94@gmail.com>
2024-05-31 09:05:10 +0200

    update code

--- RecoilCorrect/Functions.cc
+++ RecoilCorrect/Functions.cc
@@ -9,12 +9,13 @@
 #include "TList.h"
 #include <iostream>
 
-TVector2 SetMagPhi( float pt, float phi)
+TVector2 SetMagPhi(float pt, float phi)
 {
     // in the default SetMagPhi in TVector2 in root,
     // mag is set to positive without changing the direction
     // https://github.com/root-project/root/blob/master/math/physics/src/TVector2.cxx#L124
-    if( pt<0 ) phi = phi + TMath::Pi();
+    if (pt < 0)
+        phi = phi + TMath::Pi();
     TVector2 v2;
     v2.SetMagPhi(pt, phi);
     return v2;
@@ -42,167 +43,198 @@ TLorentzVector VVecM(float lep0_pt, float lep0_eta, float lep0_phi, float lep0_m
     return (vlep0 + vlep1);
 }
 
-TVector2 UVec(float leps_pt, float leps_phi, 
-              float met_pt,  float met_phi)
+TVector2 UVec(float leps_pt, float leps_phi,
+              float met_pt, float met_phi)
 {
-    TVector2 vleps = SetMagPhi( leps_pt, leps_phi );
-    TVector2 vmet  = SetMagPhi(  met_pt,  met_phi );
+    TVector2 vleps = SetMagPhi(leps_pt, leps_phi);
+    TVector2 vmet = SetMagPhi(met_pt, met_phi);
 
-    return (vleps+vmet)*(-1.0);
+    return (vleps + vmet) * (-1.0);
 }
 
 TVector2 METVec(float leps_pt, float leps_phi,
-                float uparal,      float uperp)
+                float uparal, float uperp)
 {
-    TVector2 vleps = SetMagPhi( leps_pt, leps_phi );
-    TVector2 vparal = SetMagPhi( uparal, leps_phi + TMath::Pi() );
-    TVector2 vperp = SetMagPhi( uperp,  leps_phi + TMath::Pi()/2.0 *3.0);
+    TVector2 vleps = SetMagPhi(leps_pt, leps_phi);
+    TVector2 vparal = SetMagPhi(uparal, leps_phi + TMath::Pi());
+    TVector2 vperp = SetMagPhi(uperp, leps_phi + TMath::Pi() / 2.0 * 3.0);
 
-    return (vleps+vparal+vperp)*(-1.0);
+    return (vleps + vparal + vperp) * (-1.0);
 }
 
 TVector2 METVec(float lep_pt, float lep_phi,
                 float uparal, float uperp,
                 float V_phi)
 {
-    TVector2 vlep = SetMagPhi( lep_pt, lep_phi );
-    TVector2 vparal = SetMagPhi( uparal, V_phi + TMath::Pi() );
-    TVector2 vperp  = SetMagPhi( uperp,  V_phi + TMath::Pi()/2.0*3.0);
-    return (vlep+vparal+vperp)*(-1.0);
+    TVector2 vlep = SetMagPhi(lep_pt, lep_phi);
+    TVector2 vparal = SetMagPhi(uparal, V_phi + TMath::Pi());
+    TVector2 vperp = SetMagPhi(uperp, V_phi + TMath::Pi() / 2.0 * 3.0);
+    return (vlep + vparal + vperp) * (-1.0);
 }
 
-Float_t UCorrection(float u, float zpt, TF1* f_mc_mean, TF1* f_mc_sigma, TF1* f_data_mean, TF1* f_data_sigma)
+Float_t UCorrection(float u, float zpt, TF1 *f_mc_mean, TF1 *f_mc_sigma, TF1 *f_data_mean, TF1 *f_data_sigma)
 {
     float zptmax = f_mc_mean->GetXmax();
-    if( zpt>zptmax ) return u;
-    float u_mc_mean    = f_mc_mean->Eval( zpt );
-    float u_mc_sigma   = f_mc_sigma->Eval( zpt );
-    float u_data_mean  = f_data_mean->Eval( zpt );
-    float u_data_sigma = f_data_sigma->Eval( zpt );
-    float u_corr = (u-u_mc_mean)/(u_mc_sigma+0.0001) * u_data_sigma + u_data_mean;
+    if (zpt > zptmax)
+        return u;
+    float u_mc_mean = f_mc_mean->Eval(zpt);
+    float u_mc_sigma = f_mc_sigma->Eval(zpt);
+    float u_data_mean = f_data_mean->Eval(zpt);
+    float u_data_sigma = f_data_sigma->Eval(zpt);
+    float u_corr = (u - u_mc_mean) / (u_mc_sigma + 0.0001) * u_data_sigma + u_data_mean;
     return u_corr;
 }
 
-Float_t UCorrection(float u, float zpt, TF1* f_mc_sigma, TF1* f_data_sigma)
+Float_t UCorrection(float u, float zpt, TF1 *f_mc_sigma, TF1 *f_data_sigma)
 {
     float zptmax = f_mc_sigma->GetXmax();
-    if( zpt>zptmax ) return u;
-    float u_mc_sigma   = f_mc_sigma->Eval( zpt );
-    float u_data_sigma = f_data_sigma->Eval( zpt );
-    float u_corr = u/(u_mc_sigma+0.0001) * u_data_sigma;
+    if (zpt > zptmax)
+        return u;
+    float u_mc_sigma = f_mc_sigma->Eval(zpt);
+    float u_data_sigma = f_data_sigma->Eval(zpt);
+    float u_corr = u / (u_mc_sigma + 0.0001) * u_data_sigma;
     return u_corr;
 }
 
-Float_t UCorrection_Quant(float u, float zpt, TH1F* hptbins, TList* tfs_Data, TList* tfs_MC, float qcut = 0.005, bool isU1Diff = false)
+Float_t UCorrection_Quant(float u, float zpt, TH1F *hptbins, TList *tfs_Data, TList *tfs_MC, float qcut = 0.005, bool isU1Diff = false)
 {
     float zptmax = hptbins->GetXaxis()->GetXmax();
     float zptmin = hptbins->GetXaxis()->GetXmin();
     // outlier in the z pt bin
-    if( zpt>zptmax || zpt<zptmin ) return u;
+    if (zpt > zptmax || zpt < zptmin)
+        return u;
     int index = hptbins->FindBin(zpt) - 1;
-    TF1* tf1_MC = (TF1*)tfs_MC->At(index);
-    TF1* tf1_Data = (TF1*)tfs_Data->At(index);
+    TF1 *tf1_MC = (TF1 *)tfs_MC->At(index);
+    TF1 *tf1_Data = (TF1 *)tfs_Data->At(index);
 
-    if( u > tf1_MC->GetXmax() || u < tf1_MC->GetXmin() ) return u;
-    
+    if (u > tf1_MC->GetXmax() || u < tf1_MC->GetXmin())
+        return u;
+
     float u_temp = u;
-    if (isU1Diff) {
+    if (isU1Diff)
+    {
         u_temp -= zpt;
     }
     float qt = tf1_MC->Eval(u_temp); // quantile, cdf
     // no correction on outliers
-    if(qcut>0) {
-        if( qt<qcut || qt>1-qcut ) return u;
+    if (qcut > 0)
+    {
+        if (qt < qcut || qt > 1 - qcut)
+            return u;
     }
-    float u_corr = tf1_Data->GetX( qt );
-    if (isU1Diff) {
+    float u_corr = tf1_Data->GetX(qt);
+    if (isU1Diff)
+    {
         u_corr += zpt;
     }
     return u_corr;
 }
 
-Float_t UCorrection_Quant(float u, int njet, float zpt, TH1F* hjetbins, TH1F* hptbins, TList* tfs_Data, TList* tfs_MC, float qcut = 0.005, bool isU1Diff = false)
+Float_t UCorrection_Quant(float u, int njet, float zpt, TH1F *hjetbins, TH1F *hptbins, TList *tfs_Data, TList *tfs_MC, float qcut = 0.005, bool isU1Diff = false)
 {
     float zptmax = hptbins->GetXaxis()->GetXmax();
     float njetmax = hjetbins->GetXaxis()->GetXmax();
     float zptmin = hptbins->GetXaxis()->GetXmin();
     float njetmin = hjetbins->GetXaxis()->GetXmin();
     // outlier
-    if( zpt>zptmax || njet>njetmax || zpt<zptmin || njet<njetmin ) return u;
+    if (zpt > zptmax || njet > njetmax || zpt < zptmin || njet < njetmin)
+        return u;
     int index = hptbins->FindBin(zpt) - 1;
     int njetbin = hjetbins->FindBin(njet) - 1;
-    TF1* tf1_MC = (TF1*)((TList*)tfs_MC->At(njetbin))->At(index);
-    TF1* tf1_Data = (TF1*)((TList*)tfs_Data->At(njetbin))->At(index);
+    TF1 *tf1_MC = (TF1 *)((TList *)tfs_MC->At(njetbin))->At(index);
+    TF1 *tf1_Data = (TF1 *)((TList *)tfs_Data->At(njetbin))->At(index);
 
-    if( u > tf1_MC->GetXmax() || u < tf1_MC->GetXmin() ) return u;
+    if (u > tf1_MC->GetXmax() || u < tf1_MC->GetXmin())
+        return u;
 
     float u_temp = u;
-    if (isU1Diff) {
+    if (isU1Diff)
+    {
         u_temp -= zpt;
     }
     float qt = tf1_MC->Eval(u_temp); // quantile, cdf
     // no correction on outliers
-    if(qcut>0) {
-        if( qt<qcut || qt>1-qcut ) return u;
+    if (qcut > 0)
+    {
+        if (qt < qcut || qt > 1 - qcut)
+            return u;
     }
-    float u_corr = tf1_Data->GetX( qt );
-    if (isU1Diff) {
+    float u_corr = tf1_Data->GetX(qt);
+    if (isU1Diff)
+    {
         u_corr += zpt;
     }
     return u_corr;
 }
 
-Float_t ZptReWeight(float zpt, TH1D* h_zpt_ratio_data_vs_MC, bool isData = false)
+Float_t ZptReWeight(float zpt, TH1D *h_zpt_ratio_data_vs_MC, bool isData = false)
 {
-    if( isData ) return 1.0;
+    if (isData)
+        return 1.0;
     float zptmax = h_zpt_ratio_data_vs_MC->GetXaxis()->GetXmax();
-    if( zpt>zptmax ) return 1.0;
-    return h_zpt_ratio_data_vs_MC->GetBinContent( h_zpt_ratio_data_vs_MC->FindBin(zpt) );
+    if (zpt > zptmax)
+        return 1.0;
+    return h_zpt_ratio_data_vs_MC->GetBinContent(h_zpt_ratio_data_vs_MC->FindBin(zpt));
 }
 
 // calculate mT based on lepton px, py and recoil ux, uy
-float calMT_fromUT_XY(float lep_px, float lep_py, float ux, float uy){
-   float lep_pt = TMath::Sqrt( lep_px * lep_px + lep_py * lep_py );
-   float u_pt   = TMath::Sqrt( ux * ux + uy * uy );
-   float sum_pt = TMath::Sqrt( (lep_px + ux)*(lep_px + ux) + (lep_py+uy)*(lep_py+uy) );
-   float mT2 = 2* ( lep_pt*( lep_pt + sum_pt) + lep_px*ux + lep_py*uy );
-   return TMath::Sqrt( mT2 );
+float calMT_fromUT_XY(float lep_px, float lep_py, float ux, float uy)
+{
+    float lep_pt = TMath::Sqrt(lep_px * lep_px + lep_py * lep_py);
+    float u_pt = TMath::Sqrt(ux * ux + uy * uy);
+    float sum_pt = TMath::Sqrt((lep_px + ux) * (lep_px + ux) + (lep_py + uy) * (lep_py + uy));
+    float mT2 = 2 * (lep_pt * (lep_pt + sum_pt) + lep_px * ux + lep_py * uy);
+    return TMath::Sqrt(mT2);
 }
 
-float calMT_fromUT_PtPhi(float lep_pt, float lep_phi, float ut, float uphi){
-    return calMT_fromUT_XY(lep_pt*TMath::Cos(lep_phi), lep_pt*TMath::Sin(lep_phi), ut*TMath::Cos(uphi), ut*TMath::Sin(uphi));
+float calMT_fromUT_PtPhi(float lep_pt, float lep_phi, float ut, float uphi)
+{
+    return calMT_fromUT_XY(lep_pt * TMath::Cos(lep_phi), lep_pt * TMath::Sin(lep_phi), ut * TMath::Cos(uphi), ut * TMath::Sin(uphi));
 }
 
-float calMT_fromMET_XY(float lep_px, float lep_py, float met_x, float met_y){
+float calMT_fromMET_XY(float lep_px, float lep_py, float met_x, float met_y)
+{
     float ux = -(lep_px + met_x);
     float uy = -(lep_py + met_y);
     return calMT_fromUT_XY(lep_px, lep_py, ux, uy);
 }
 
-float calMT_fromMET_PtPhi(float lep_pt, float lep_phi, float met_pt, float met_phi){
-    return calMT_fromMET_XY( lep_pt*TMath::Cos(lep_phi), lep_pt*TMath::Sin(lep_phi), met_pt*TMath::Cos(met_phi), met_pt*TMath::Sin(met_phi) );
+float calMT_fromMET_PtPhi(float lep_pt, float lep_phi, float met_pt, float met_phi)
+{
+    return calMT_fromMET_XY(lep_pt * TMath::Cos(lep_phi), lep_pt * TMath::Sin(lep_phi), met_pt * TMath::Cos(met_phi), met_pt * TMath::Sin(met_phi));
 }
 
-float calUT_XY(float lep_px, float lep_py, float met_x, float met_y){
-    float ux = -(lep_px + met_x );
-    float uy = -(lep_py + met_y );
-    return TMath::Sqrt(ux*ux + uy*uy);
+float calUT_XY(float lep_px, float lep_py, float met_x, float met_y)
+{
+    float ux = -(lep_px + met_x);
+    float uy = -(lep_py + met_y);
+    return TMath::Sqrt(ux * ux + uy * uy);
 }
 
-float calUT_PtPhi(float lep_pt, float lep_phi, float met_pt, float met_phi){
-    return calUT_XY( lep_pt*TMath::Cos(lep_phi), lep_pt*TMath::Sin(lep_phi), met_pt*TMath::Cos(met_phi), met_pt*TMath::Sin(met_phi) );
+float calUT_PtPhi(float lep_pt, float lep_phi, float met_pt, float met_phi)
+{
+    return calUT_XY(lep_pt * TMath::Cos(lep_phi), lep_pt * TMath::Sin(lep_phi), met_pt * TMath::Cos(met_phi), met_pt * TMath::Sin(met_phi));
 }
 
 Int_t trueWIndex(int trueWn)
 {
-    if(trueWn==1){
+    if (trueWn == 1)
+    {
         return 0;
     }
-    else if(trueWn==2){
+    else if (trueWn == 2)
+    {
         return 1;
     }
-    else{
+    else
+    {
         return -1;
     }
 }
+
+float applyXYCorr(float met_x, float npv, TF1 *f1)
+{
+    return met_x - f1->Eval(npv);
+}
+
 #endif
---
+++ RecoilCorrect/met_xy_dataMC.py
@@ -0,0 +1,253 @@
+import ROOT
+import sys, os
+sys.path.append("../RecoilResol/CMSPLOTS")
+sys.path.append("../RecoilResol/")
+from CMSPLOTS.myFunction import DrawHistos
+from collections import OrderedDict
+from utils.utils import getnVtxBins
+import argparse
+
+noLumi = False
+writeOutput = False
+#nPV = "PV_npvsGood"
+nPV = "PV_npvs"
+
+ROOT.gROOT.SetBatch(True)
+ROOT.ROOT.EnableImplicitMT(10)
+ROOT.gSystem.Load("Functions_cc.so")
+
+outdir = f"plots/MET_xy"
+if not os.path.exists(outdir):
+   print(f"Creating {outdir}")
+   os.makedirs(outdir)
+
+chain = ROOT.TChain("Events")
+chain.Add("/afs/cern.ch/work/y/yofeng/public/outputroot/Data.root")
+rdf_data = ROOT.ROOT.RDataFrame(chain)
+
+branches_data = list(rdf_data.GetColumnNames())
+
+chainMC = ROOT.TChain("Events")
+chainMC.Add("/afs/cern.ch/work/y/yofeng/public/outputroot/DY.root")
+rdf_MC = ROOT.ROOT.RDataFrame(chainMC)
+
+branches_MC = list(rdf_MC.GetColumnNames())
+
+def prepareVars(rdf):
+   rdf = rdf.Define("pT_muons", "Z_pt").Define("phi_muons", "Z_phi")
+   rdf = rdf.Define("PF_x", "MET_pt*TMath::Cos(MET_phi)") \
+            .Define("PF_y", "MET_pt*TMath::Sin(MET_phi)") \
+            .Define("PF_phi", "MET_phi") \
+            .Define("PUPPI_x", "PuppiMET_pt*TMath::Cos(PuppiMET_phi)") \
+            .Define("PUPPI_y", "PuppiMET_pt*TMath::Sin(PuppiMET_phi)") \
+            .Define("PUPPI_phi", "PuppiMET_phi") \
+            .Define("DeepMET_x", "DeepMETResolutionTune_pt*TMath::Cos(DeepMETResolutionTune_phi)") \
+            .Define("DeepMET_y", "DeepMETResolutionTune_pt*TMath::Sin(DeepMETResolutionTune_phi)") \
+            .Define("DeepMET_phi", "DeepMETResolutionTune_phi")
+   return rdf
+
+rdf_data = prepareVars(rdf_data)
+rdf_MC = prepareVars(rdf_MC)
+
+
+recoils = ["PF", "PUPPI", "DeepMET"]
+
+colors = {
+            "PF": 1,
+            "PUPPI": 2,
+            "GEN": 6,
+            "DeepMET": 4,
+            "DeepMETCorr": 8,
+         }
+
+labels = {
+            "TK": "TK",
+            "PF": "PF",
+            "PUPPI": "PUPPI",
+            "GEN": "GEN",
+            "TKPHO": "TK+Photon",
+            "DeepMET": "DeepMET",
+            "DeepMETCorr": "DeepMET",
+         }
+
+linestyles = {}
+
+for itype in recoils:
+   colors[itype + "_MC"] = colors[itype]
+
+   linestyles[itype] = 1
+   linestyles[itype + "_MC"] = 2
+   
+   
+def deriveXYCorr(oname = "MET_xy_dataMC.root", suffix = "PreXYCorr"):
+   """
+   derive the XY corrections for the MET
+   and save the output in a root file
+   
+   profile the MET in x and y components
+   and fit the profiles with a pol1
+   as a function of nVtx
+   """
+
+   hprofs_data = OrderedDict()
+   hprofs_MC = OrderedDict()
+   
+   hphis_data = OrderedDict()
+   hphis_MC = OrderedDict()
+
+   for ax in ["x", "y"]:
+      hprofs_data[ax] = OrderedDict()
+      hprofs_MC[ax] = OrderedDict()
+   
+   for itype in recoils:
+      for ax in ["x", "y"]:
+         hname = f"hprof_{itype}_{ax}_vs_nVtx_{suffix}"
+         hname_data = f"{hname}_data"
+         hname_MC = f"{hname}_MC"
+         hprofs_data[ax][itype] = rdf_data.Profile1D((hname_data, hname_data, 70, 0, 70), nPV, itype + "_" + ax)
+         hprofs_MC[ax][itype] = rdf_MC.Profile1D((hname_MC, hname_MC, 70, 0, 70), nPV, itype + "_" + ax)
+         
+      hphi_name = f"hphi_{itype}_{suffix}"
+      hphi_name_data = f"{hphi_name}_data"
+      hphi_name_MC = f"{hphi_name}_MC"
+      hphis_data[itype] = rdf_data.Histo1D((hphi_name_data, f"{itype}_phi", 100, -3.2, 3.2), itype + "_phi")
+      hphis_MC[itype] = rdf_MC.Histo1D((hphi_name_MC, f"{itype}_phi", 100, -3.2, 3.2), itype + "_phi")
+
+   # fit the profiles with a pol1
+   tfs_to_save = []
+   for itype in recoils:
+      for ax in ["x", "y"]:
+         fname = f"tf1_{itype}_{ax}_vs_nVtx"
+         fname_data = f"{fname}_data"
+         fname_MC = f"{fname}_MC"
+
+         tf1_data = ROOT.TF1(fname_data, "pol1", 0, 70)
+         hprofs_data[ax][itype].Fit(tf1_data, "R")
+         tf1_MC = ROOT.TF1(fname_MC, "pol1", 0, 70)
+         hprofs_MC[ax][itype].Fit(tf1_MC, "R")
+         
+         hname = f"hprof_{itype}_{ax}_vs_nVtx_{suffix}"
+
+         DrawHistos([hprofs_data[ax][itype], hprofs_MC[ax][itype], tf1_data, tf1_MC], [labels[itype] + " Data", labels[itype] + " MC"], 0, 70, "nPV", -10.0, 10.0, f"<MET_{ax}> [GeV]", hname, drawashist=True, dology=False, legendPos=[0.20, 0.70, 0.50, 0.90], mycolors=[colors[itype]]*4, linestyles = [linestyles[itype], linestyles[itype + "_MC"], 6, 6], noLumi=noLumi, outdir=outdir)
+
+         tfs_to_save.append(tf1_data)
+         tfs_to_save.append(tf1_MC)
+         
+      hphi_name = f"hphi_{itype}_{suffix}"
+      
+      DrawHistos([hphis_data[itype], hphis_MC[itype]], [labels[itype] + " Data", labels[itype] + " MC"], -3.2, 3.2, "#phi", 0, 0.027, "A.U.", hphi_name, drawashist=True, dology=False, legendPos=[0.20, 0.70, 0.50, 0.90], mycolors=[colors[itype], colors[itype + "_MC"]], linestyles = [linestyles[itype], linestyles[itype + "_MC"]], noLumi=noLumi, outdir=outdir, donormalize=True)
+   
+            
+   DrawHistos([hphis_data[itype] for itype in recoils] + [hphis_MC[itype] for itype in recoils], [labels[itype] for itype in recoils], -3.2, 3.2, "#phi", 0, 0.027, "A.U.", f"hphi_{suffix}", drawashist=True, dology=False, legendPos=[0.20, 0.70, 0.50, 0.90], mycolors=[colors[itype] for itype in recoils]*2, linestyles = [linestyles[itype] for itype in recoils] + [linestyles[itype + "_MC"] for itpe in recoils], noLumi=noLumi, outdir=outdir, donormalize=True)
+
+   # save the output fits
+   ofile = ROOT.TFile(oname, "recreate")
+   for itf in tfs_to_save:
+      itf.Write()
+   ofile.Close()
+   
+   return
+   
+   
+def applyXYCorr(corrname, suffix = "PostXYCorr", writeOutput = False):
+   """
+   apply XY corrections to the MET 
+   (by subtracting the profiled XY from the MET components)
+   and save the corrected output in a new file
+   """
+   ROOT.gROOT.ProcessLine(f"TFile* ifile = TFile::Open(\"{corrname}\")")
+   for itype in recoils:
+      for ax in ["x", "y"]:
+         ROOT.gROOT.ProcessLine(f"TF1* tf1_{itype}_{ax}_vs_nVtx_data = (TF1*)ifile->Get(\"tf1_{itype}_{ax}_vs_nVtx_data\")")
+         ROOT.gROOT.ProcessLine(f"TF1* tf1_{itype}_{ax}_vs_nVtx_MC = (TF1*)ifile->Get(\"tf1_{itype}_{ax}_vs_nVtx_MC\")")
+         
+   def applyCorr(rdf, dftype):
+      for itype in recoils:
+         for ax in ["x", "y"]:
+            rdf = rdf.Define(f"{itype}_{ax}_XYCorr", f"applyXYCorr({itype}_{ax}, {nPV}, tf1_{itype}_{ax}_vs_nVtx_{dftype})")
+         rdf = rdf.Define(f"{itype}_pt_XYCorr", f"TMath::Sqrt({itype}_x_XYCorr*{itype}_x_XYCorr + {itype}_y_XYCorr*{itype}_y_XYCorr)")
+         rdf = rdf.Define(f"{itype}_phi_XYCorr", f"TVector2::Phi_mpi_pi(TMath::ATan2({itype}_y_XYCorr, {itype}_x_XYCorr))")
+      return rdf
+            
+   global rdf_data
+   global rdf_MC
+   rdf_data = applyCorr(rdf_data, "data")
+   rdf_MC = applyCorr(rdf_MC, "MC")
+   
+   hprofs_data = OrderedDict()
+   hprofs_MC = OrderedDict()
+   
+   hphis_data = OrderedDict()
+   hphis_MC = OrderedDict()
+
+   for ax in ["x", "y"]:
+      hprofs_data[ax] = OrderedDict()
+      hprofs_MC[ax] = OrderedDict()
+   
+   for itype in recoils:
+      for ax in ["x", "y"]:
+         hname = f"hprof_{itype}_{ax}_vs_nVtx_{suffix}"
+         hname_data = f"{hname}_data"
+         hname_MC = f"{hname}_MC"
+         hprofs_data[ax][itype] = rdf_data.Profile1D((hname_data, hname_data, 70, 0, 70), nPV, itype + "_" + ax + "_XYCorr")
+         hprofs_MC[ax][itype] = rdf_MC.Profile1D((hname_MC, hname_MC, 70, 0, 70), nPV, itype + "_" + ax + "_XYCorr")
+         
+      hphi_name = f"hphi_{itype}_{suffix}"
+      hphi_name_data = f"{hphi_name}_data"
+      hphi_name_MC = f"{hphi_name}_MC"
+      hphis_data[itype] = rdf_data.Histo1D((hphi_name_data, f"{itype}_phi", 100, -3.2, 3.2), itype + "_phi_XYCorr")
+      hphis_MC[itype] = rdf_MC.Histo1D((hphi_name_MC, f"{itype}_phi", 100, -3.2, 3.2), itype + "_phi_XYCorr")
+
+   # fit the profiles with a pol1
+   for itype in recoils:
+      for ax in ["x", "y"]:
+         hname = f"hprof_{itype}_{ax}_vs_nVtx_{suffix}"
+         DrawHistos([hprofs_data[ax][itype], hprofs_MC[ax][itype]], [labels[itype] + " Data", labels[itype] + " MC"], 0, 70, "nPV", -10.0, 10.0, f"<MET_{ax}> [GeV]", hname, drawashist=True, dology=False, legendPos=[0.20, 0.70, 0.50, 0.90], mycolors=[colors[itype]]*2, linestyles = [linestyles[itype], linestyles[itype + "_MC"]], noLumi=noLumi, outdir=outdir)
+
+      hphi_name = f"hphi_{itype}_{suffix}"
+      DrawHistos([hphis_data[itype], hphis_MC[itype]], [labels[itype] + " Data", labels[itype] + " MC"], -3.2, 3.2, "#phi", 0, 0.019, "A.U.", hphi_name, drawashist=True, dology=False, legendPos=[0.20, 0.70, 0.50, 0.90], mycolors=[colors[itype], colors[itype + "_MC"]], linestyles = [linestyles[itype], linestyles[itype + "_MC"]], noLumi=noLumi, outdir=outdir, donormalize=True)
+      
+   DrawHistos([hphis_data[itype] for itype in recoils] + [hphis_MC[itype] for itype in recoils], [labels[itype] for itype in recoils], -3.2, 3.2, "#phi", 0, 0.019, "A.U.", f"hphi_{suffix}", drawashist=True, dology=False, legendPos=[0.20, 0.70, 0.50, 0.90], mycolors=[colors[itype] for itype in recoils]*2, linestyles = [linestyles[itype] for itype in recoils] + [linestyles[itype + "_MC"] for itpe in recoils], noLumi=noLumi, outdir=outdir, donormalize=True)
+      
+   if writeOutput:
+      def regularizeNames(rdf):
+         rdf = rdf.Define("MET_XYCorr_pt", "PF_pt_XYCorr") \
+                  .Define("MET_XYCorr_phi", "PF_phi_XYCorr") \
+                  .Define("PuppiMET_XYCorr_pt", "PUPPI_pt_XYCorr") \
+                  .Define("PuppiMET_XYCorr_phi", "PUPPI_phi_XYCorr") \
+                  .Define("DeepMETResolutionTune_XYCorr_pt", "DeepMET_pt_XYCorr") \
+                  .Define("DeepMETResolutionTune_XYCorr_phi", "DeepMET_phi_XYCorr")
+         return rdf
+   
+      rdf_data = regularizeNames(rdf_data)
+      rdf_MC = regularizeNames(rdf_MC)
+      
+      global branches_data
+      global branches_MC
+      branches_data += ["MET_XYCorr_pt", "MET_XYCorr_phi", "PuppiMET_XYCorr_pt", "PuppiMET_XYCorr_phi", "DeepMETResolutionTune_XYCorr_pt", "DeepMETResolutionTune_XYCorr_phi"]
+      branches_MC += ["MET_XYCorr_pt", "MET_XYCorr_phi", "PuppiMET_XYCorr_pt", "PuppiMET_XYCorr_phi", "DeepMETResolutionTune_XYCorr_pt", "DeepMETResolutionTune_XYCorr_phi"]
+    
+      odir = "/afs/cern.ch/work/y/yofeng/public/outputroot_withXYCorr"
+      if not os.path.exists(odir):
+         print(f"Creating {odir}")
+         os.makedirs(odir)
+         
+      rdf_data.Snapshot("Events", f"{odir}/Data.root", branches_data)
+      rdf_MC.Snapshot("Events", f"{odir}/DY.root", branches_MC)
+         
+   
+   return
+   
+   
+if __name__ == "__main__":
+   corrname = "results/MET_xy_dataMC.root"
+   
+   if 1:
+      print(f"Deriving XY corrections for MET and saving them in {corrname}")
+      deriveXYCorr(corrname)
+      print("\n\n\n")
+      
+   if 1:
+      print(f"Applying XY corrections to MET and saving the corrected output in {corrname}")
+      applyXYCorr(corrname, writeOutput=writeOutput)
+      print("\n\n\n")
