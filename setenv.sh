8dfc32640cf8ce345883f80b94b1f0ffa4efb8bd
Yongbin Feng <ybfeng94@gmail.com>
2025-03-20 16:33:55 +0100

    update env

--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,3 @@
 #source /cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc9-opt/setup.sh
-source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-centos7-gcc12-opt/setup.sh
+#source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-centos7-gcc12-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-el9-gcc13-opt/setup.sh
